e620c8b3caf2ad2d1e968c714cd39da406779f7b
Felipe Nathan <ncampigoto@gmail.com>
2022-01-13 00:18:44 -0300

    Testing more logs

--- src/main.py
+++ src/main.py
@@ -44,6 +44,30 @@ def home():
 def home_me():
     return "<p>Hello Felipe</p>"
 
+@app.route("/error/capture/exception")
+def handle_exception():
+    try:
+        1/0
+    except ZeroDivisionError:
+        apm.capture_exception()
+
+    return "<p>error captured</p>"
+
+@app.route("/error/capture/message")
+def handle_message():
+    apm.capture_message("handling error")
+
+    return "<p>error captured</p>"
+
+@app.route("/error/capture/extra")
+def handle_extra():
+    try:
+        1/0
+    except ZeroDivisionError:
+        app.logger.error("Math division", exc_info=True, extra={ 'good_at_math': False })
+
+    return "<p>error captured</p>"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
